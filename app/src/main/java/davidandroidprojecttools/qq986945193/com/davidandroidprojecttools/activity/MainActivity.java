930cbca3dc8f8dfddc294c6ad94da98c204af1fa
David Zheng <986945193@qq.com>
2016-09-24 11:16:07 +0800

    mainactivity 添加注释主类。

--- app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/activity/MainActivity.java
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/activity/MainActivity.java
@@ -4,7 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.R;
+/**
+ * @author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @交流Qq ：986945193
+ * @GitHub: https://github.com/QQ986945193
+ */
 
+/**
+ * 首次进入的主类
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
