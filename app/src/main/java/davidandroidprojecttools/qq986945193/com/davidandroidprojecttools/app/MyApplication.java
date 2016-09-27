dd945f26f7c78386496370312addf0735f2fdc75
Administrator <admin>
2016-09-27 14:00:03 +0800

    配置application

---
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/app/MyApplication.java
@@ -0,0 +1,20 @@
+package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.app;
+
+import android.app.Application;
+/**
+ * @author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @交流Qq ：986945193
+ * @GitHub: https://github.com/QQ986945193
+ */
+
+/**
+ * Application的配置
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
