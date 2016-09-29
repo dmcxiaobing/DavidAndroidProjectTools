350a853e57d2af04ce1fd63f126294d3d3488839
Administrator <admin>
2016-09-29 13:45:53 +0800

    判断网络状态的工具类

---
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/utils/NetworkAvailableUtils.java
@@ -0,0 +1,112 @@
+package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.utils;
+
+import android.content.Context;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+import java.util.List;
+
+/**
+ * @author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @交流Qq ：986945193
+ * @GitHub: https://github.com/QQ986945193
+ */
+
+public class NetworkAvailableUtils {
+
+    /**
+     * 判断是否联网
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm == null) {
+        } else {
+            // 　　　　　　如果仅仅是用来判断网络连接
+            // 　　　　　　则可以使用 cm.getActiveNetworkInfo().isAvailable();
+            NetworkInfo[] info = cm.getAllNetworkInfo();
+            if (info != null) {
+                for (int i = 0; i < info.length; i++) {
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判断GPS是否打开
+     *
+     * @param context
+     * @return
+     */
+
+    public static boolean isGpsEnabled(Context context) {
+        LocationManager lm = ((LocationManager) context
+                .getSystemService(Context.LOCATION_SERVICE));
+        List<String> accessibleProviders = lm.getProviders(true);
+        return accessibleProviders != null && accessibleProviders.size() > 0;
+    }
+
+    /**
+     * 三、判断WIFI是否打开
+     *
+     * @param context
+     * @return
+     */
+
+    public static boolean isWifiEnabled(Context context) {
+        ConnectivityManager mgrConn = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        TelephonyManager mgrTel = (TelephonyManager) context
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        return ((mgrConn.getActiveNetworkInfo() != null && mgrConn
+                .getActiveNetworkInfo().getState() == NetworkInfo.State.CONNECTED) || mgrTel
+                .getNetworkType() == TelephonyManager.NETWORK_TYPE_UMTS);
+    }
+
+    /**
+     * 四、判断是否是3G网络
+     *
+     * @param context
+     * @return
+     */
+
+    public static boolean is3rd(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkINfo = cm.getActiveNetworkInfo();
+        if (networkINfo != null
+                && networkINfo.getType() == ConnectivityManager.TYPE_MOBILE) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 五、判断是wifi还是3g网络,用户的体现性在这里了，wifi就可以建议下载或者在线播放。
+     *
+     * @param context
+     * @return
+     */
+
+    public static boolean isWifi(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkINfo = cm.getActiveNetworkInfo();
+        if (networkINfo != null
+                && networkINfo.getType() == ConnectivityManager.TYPE_WIFI) {
+            return true;
+        }
+        return false;
+    }
+}
