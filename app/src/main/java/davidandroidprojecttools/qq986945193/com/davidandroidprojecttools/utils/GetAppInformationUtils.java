253ca4a1db0a846674637f10ff3efe68fdeaa54d
Administrator <admin>
2016-09-29 12:11:53 +0800

    获得版本信息的工具方法

---
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/utils/GetAppInformationUtils.java
@@ -0,0 +1,45 @@
+package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.utils;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+/**
+ * @author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @交流Qq ：986945193
+ * @GitHub: https://github.com/QQ986945193
+ */
+
+/**
+ * 获得版本信息的工具方法
+ */
+public class GetAppInformationUtils {
+
+    //版本名
+    public static String getVersionName(Context context) {
+        return getPackageInfo(context).versionName;
+    }
+
+    //版本号
+    public static int getVersionCode(Context context) {
+        return getPackageInfo(context).versionCode;
+    }
+
+    private static PackageInfo getPackageInfo(Context context) {
+        PackageInfo pi = null;
+
+        try {
+            PackageManager pm = context.getPackageManager();
+            pi = pm.getPackageInfo(context.getPackageName(),
+                    PackageManager.GET_CONFIGURATIONS);
+
+            return pi;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return pi;
+    }
+
+}
