ada1149aee212c81ca9e93804741b20062a01d52
Administrator <admin>
2016-09-29 14:03:04 +0800

    添加volley jar文件，以及px sp dp相互转换的工具类

---
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/utils/DensityUtils.java
@@ -0,0 +1,63 @@
+package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.utils;
+/**
+ * @author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @交流Qq ：986945193
+ * @GitHub: https://github.com/QQ986945193
+ */
+
+import android.content.Context;
+import android.util.TypedValue;
+
+/**
+ * dp  px sp 相互转换的工具类
+ */
+public class DensityUtils {
+    /**
+     * dp转px
+     *
+     * @param context
+     * @param
+     * @return
+     */
+    public static int dp2px(Context context, float dpVal) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                dpVal, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * sp转px
+     *
+     * @param context
+     * @param
+     * @return
+     */
+    public static int sp2px(Context context, float spVal) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
+                spVal, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * px转dp
+     *
+     * @param context
+     * @param pxVal
+     * @return
+     */
+    public static float px2dp(Context context, float pxVal) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (pxVal / scale);
+    }
+
+    /**
+     * px转sp
+     *
+     * @param
+     * @param pxVal
+     * @return
+     */
+    public static float px2sp(Context context, float pxVal) {
+        return (pxVal / context.getResources().getDisplayMetrics().scaledDensity);
+    }
+}
