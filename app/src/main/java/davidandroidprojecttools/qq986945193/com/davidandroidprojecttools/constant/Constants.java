392bdac0ac73345ce25170f943a87cfac84960a9
Administrator <admin>
2016-10-07 18:22:33 +0800

    提交常量类

---
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/constant/Constants.java
@@ -0,0 +1,17 @@
+package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.constant;
+
+/**
+ * @author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @交流Qq ：986945193
+ * @GitHub: https://github.com/QQ986945193
+ */
+
+/**
+ * 存放常量值得类
+ */
+public class Constants {
+
+
+}
