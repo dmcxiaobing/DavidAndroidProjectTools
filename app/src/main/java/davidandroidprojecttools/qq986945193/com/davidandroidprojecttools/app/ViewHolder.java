49f8921bd9b7955b865a23f5799a5d42a03b31e0
Administrator <admin>
2016-10-12 10:40:08 +0800

    仿淘宝,百度外卖,饿了么等头条滚动效果

--- app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/app/ViewHolder.java
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/app/ViewHolder.java
@@ -5,7 +5,16 @@ import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+/**
+ * @author ：程序员小冰
+ *
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ *
+ * @GitHub: https://github.com/QQ986945193
+ *
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ *
+ */
 public class ViewHolder {
 
 	private SparseArray<View> mViews;
