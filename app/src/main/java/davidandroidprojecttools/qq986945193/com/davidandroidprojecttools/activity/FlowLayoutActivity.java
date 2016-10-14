00d1cc9546cccd87c1521aa389d5e3aea8fcb689
qq986945193 <986945193@qq.com>
2016-10-14 18:06:16 +0800

    添加流式布局

---
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/activity/FlowLayoutActivity.java
@@ -0,0 +1,21 @@
+package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.activity;
+
+import davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.R;
+
+/**
+ * @author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ */
+public class FlowLayoutActivity extends BaseActivity {
+    @Override
+    protected void initView() {
+        setContentView(R.layout.activity_flowlayout);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
