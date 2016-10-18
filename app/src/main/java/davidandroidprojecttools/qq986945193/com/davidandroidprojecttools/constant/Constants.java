cb16306b5fef7b923d8ff24a23b6ecb155e8527d
qq986945193 <986945193@qq.com>
2016-10-18 18:08:17 +0800

    添加tab

--- app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/constant/Constants.java
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/constant/Constants.java
@@ -17,7 +17,7 @@ public class Constants {
     /*这里是使用百度api的时候，需要传递的参数名*/
     public static final String API_KEY = "apikey";
     /*这里是使用百度API的时候所要传递的自己的秘钥*/
-    public static final String API_KEY_SECRET = "3ecfce74d2c88190c6568948db5d2018";
+    public static final String API_KEY_SECRET = "73ecfce74d2c88190c6568948db5d2018";
 
     public static final String LOADING_DATA = "加载中...";
 }
