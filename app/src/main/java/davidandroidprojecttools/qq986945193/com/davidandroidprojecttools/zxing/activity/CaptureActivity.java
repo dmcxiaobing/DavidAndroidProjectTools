6c4ea701b35bf05a195503c1a54d373db557199a
qq986945193 <986945193@qq.com>
2016-10-20 09:49:49 +0800

    添加注释

--- app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/zxing/activity/CaptureActivity.java
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/zxing/activity/CaptureActivity.java
@@ -14,7 +14,16 @@
  * limitations under the License.
  */
 package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.zxing.activity;
-
+/**
+ * @author ：程序员小冰
+ *
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ *
+ * @GitHub: https://github.com/QQ986945193
+ *
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ *
+ */
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
