59c0677058a8a1a4348d692e3ba25ffcff2b332e
qq986945193 <986945193@qq.com>
2016-10-30 10:16:52 +0800

    带有自动加载与上拉刷新的listview

---
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/activity/PullToRefreshAndLoadMoreActivity.java
@@ -0,0 +1,23 @@
+package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.activity;
+
+/**
+ * @author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ */
+
+/**
+ * 带有自动加载与上拉刷新的listview
+ */
+public class PullToRefreshAndLoadMoreActivity extends BaseActivity {
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
