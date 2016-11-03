2e2a9d45f54d61ec48b454c65ddcfa8c7d1afbea
qq986945193 <986945193@qq.com>
2016-11-03 18:00:34 +0800

    程序员小冰

--- app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/bean/CityModelBean.java
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/bean/CityModelBean.java
@@ -2,7 +2,13 @@ package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.bean;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * @Author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @OsChina空间: https://my.oschina.net/mcxiaobing
+ */
 public class CityModelBean {
     private String name;
     private List<DistrictModelBean> districtList;
--- app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/bean/ProvinceBean.java
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/bean/ProvinceBean.java
@@ -1,7 +1,11 @@
 package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.bean;
 
 /**
- * Created by Sai on 15/11/22.
+ * @Author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @OsChina空间: https://my.oschina.net/mcxiaobing
  */
 public class ProvinceBean {
     private String name;
