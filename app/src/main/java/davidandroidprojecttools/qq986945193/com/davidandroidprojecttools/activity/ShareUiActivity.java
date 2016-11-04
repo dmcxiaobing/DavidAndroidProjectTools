89955cf074a59d90c3f1531610e0e629b5951fae
qq986945193 <986945193@qq.com>
2016-11-04 15:06:53 +0800

    程序员小冰

--- app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/activity/ShareUiActivity.java
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/activity/ShareUiActivity.java
@@ -1,18 +1,19 @@
 package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.activity;
 
-import davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.R;
-
 /**
- * @author ：程序员小冰
+ * @Author ：程序员小冰
  * @新浪微博 ：http://weibo.com/mcxiaobing
  * @GitHub: https://github.com/QQ986945193
  * @CSDN博客: http://blog.csdn.net/qq_21376985
  * @OsChina空间: https://my.oschina.net/mcxiaobing
  */
-public class ShareDialogViewActivity extends BaseActivity {
+
+/**
+ * 自定义分享界面的UI功能实现与详解
+ */
+public class ShareUiActivity extends BaseActivity {
     @Override
     protected void initView() {
-        setContentView(R.layout.activity_share_dialog_view);
 
     }
 
