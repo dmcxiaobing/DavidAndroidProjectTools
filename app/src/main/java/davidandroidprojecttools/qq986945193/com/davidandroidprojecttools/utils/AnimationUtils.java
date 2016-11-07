b41f5ed68a4aa08d38914aa5bce8956ce34eca66
qq986945193 <986945193@qq.com>
2016-11-07 10:24:56 +0800

    添加自定义分享的ui界面

--- app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/utils/AnimationUtils.java
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/utils/AnimationUtils.java
@@ -9,8 +9,12 @@ import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 
 /**
- * AnimationHelper
+ * @author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
  */
+
 public class AnimationUtils {
     /**
      * 进入动画的时间
--- app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/utils/ViewUtils.java
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/utils/ViewUtils.java
@@ -1,6 +1,12 @@
 package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.utils;
 
 import android.view.View;
+/**
+ * @author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ */
 
 public class ViewUtils {
 
