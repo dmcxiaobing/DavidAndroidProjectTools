c65dd44b173772904d080639034202439bbbc519
qq986945193 <986945193@qq.com>
2016-11-07 13:47:04 +0800

    /**程序员小冰*/

--- app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/utils/LoadingDialog.java
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/utils/LoadingDialog.java
@@ -11,9 +11,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.R;
-
 /**
- * Created by Administrator on 14-11-23.
+ * @author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
  */
 public class LoadingDialog extends Dialog {
     public LoadingDialog(Context context) {
