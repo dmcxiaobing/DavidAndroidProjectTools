904098b574b6b7181cafa44869a962379019237f
qq986945193 <986945193@qq.com>
2016-11-12 18:12:00 +0800

    添加关于作者的信息

--- app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/constant/Constants.java
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/constant/Constants.java
@@ -17,11 +17,11 @@ public class Constants {
     /*这里是使用百度api的时候，需要传递的参数名*/
     public static final String API_KEY = "apikey";
     /*这里是使用百度API的时候所要传递的自己的秘钥*/
-    public static final String API_KEY_SECRET = "";
+    public static final String API_KEY_SECRET = "73ecfce74d2c88190c6568948db5d2018";
     /*获取网络数据请求中dialog的显示提示*/
     public static final String LOADING_DATA = "加载中...";
     /*腾讯Bugly检测app升级的SDK  注册时申请的APPID*/
-    public static final String TENCENT_BUGLY_APP_ID = "";
+    public static final String TENCENT_BUGLY_APP_ID = "9000581437";
     /*腾讯Bugly检测app升级的SDK  注册时申请的APPID*/
     public static final String BASE_ACTIVITY_TITLE = "项目中有些功能无法进行演示，具体实现方法请看源代码";
 }
