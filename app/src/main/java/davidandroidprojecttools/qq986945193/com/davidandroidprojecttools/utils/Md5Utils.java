c3c038bc038cee99f7a59997d2d701f185b4aa5e
qq986945193 <986945193@qq.com>
2016-12-09 18:25:01 +0800

    添加MD5工具类

---
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/utils/Md5Utils.java
@@ -0,0 +1,52 @@
+package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.utils;
+
+import java.security.MessageDigest;
+/*
+ * @Author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @OsChina空间: https://my.oschina.net/mcxiaobing
+ */
+
+/**
+ * ----------------------------------------------------------------------------
+ * 功能描述：MD5加密,java String 转变成MD5 String
+ * ----------------------------------------------------------------------------
+ */
+public class Md5Utils {
+    /**
+     * MD5加码。32位
+     *
+     * @param inStr
+     * @return
+     */
+    public static String MD5(String inStr) {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+            return "";
+        }
+        char[] charArray = inStr.toCharArray();
+        byte[] byteArray = new byte[charArray.length];
+
+        for (int i = 0; i < charArray.length; i++)
+            byteArray[i] = (byte) charArray[i];
+
+        byte[] md5Bytes = md5.digest(byteArray);
+
+        StringBuffer hexValue = new StringBuffer();
+
+        for (int i = 0; i < md5Bytes.length; i++) {
+            int val = ((int) md5Bytes[i]) & 0xff;
+            if (val < 16)
+                hexValue.append("0");
+            hexValue.append(Integer.toHexString(val));
+        }
+
+        return hexValue.toString();
+    }
+}
