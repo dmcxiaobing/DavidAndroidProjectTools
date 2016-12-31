89da3444fdd77c4862b46b8d9e592dac34859952
qq986945193 <986945193@qq.com>
2016-12-31 15:52:52 +0800

    spinner(下拉框)的知识点详解

---
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/activity/SpinnerActivity.java
@@ -0,0 +1,25 @@
+package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.activity;/*
+ * @Author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @OsChina空间: https://my.oschina.net/mcxiaobing
+ */
+
+import davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.R;
+
+/**
+ * spinner(下拉框)的知识点详解
+ */
+public class SpinnerActivity extends BaseActivity {
+    @Override
+    protected void initView() {
+        setContentView(R.layout.activity_spinner);
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
