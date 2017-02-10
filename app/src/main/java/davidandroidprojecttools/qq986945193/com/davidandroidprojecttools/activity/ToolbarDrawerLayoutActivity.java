0c2e4ab92e9e338c1de9a28e0d4726296b3059f6
qq986945193 <986945193@qq.com>
2017-02-10 13:48:26 +0800

    toolbar与drawerlayout的使用详解

--- app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/activity/ToolbarDrawerLayoutActivity.java
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/activity/ToolbarDrawerLayoutActivity.java
@@ -10,11 +10,18 @@ package davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.activi
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.R;
+import davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.utils.ToastUtils;
 
 /**
  * Toolbar和DrawableLayout的用法
@@ -22,16 +29,83 @@ import davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.R;
 public class ToolbarDrawerLayoutActivity extends AppCompatActivity {
     private Toolbar toolbar;
 
+    private DrawerLayout drawerlayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+
+    private Button btnOne;
+    private Button btnTwo;
+    private Button btnThree;
+    private Button btnFour;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar_drawerlayout);
+        initToolbar();
+
+    }
+
+    private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         //设置支持toolbar
         setSupportActionBar(toolbar);
+
+
+        btnOne = (Button) findViewById(R.id.btn_one);
+        btnTwo = (Button) findViewById(R.id.btn_two);
+        btnThree = (Button) findViewById(R.id.btn_three);
+        btnFour = (Button) findViewById(R.id.btn_four);
+
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        setToolbarBackMethod();
+        //设置toolbar的监听事件与图表
+//        setToolbarBackMethod();
+
+        /**
+         * 设置drawerlayout
+         */
+        drawerlayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerlayout, toolbar, 0, 0);
+        actionBarDrawerToggle.syncState();
+        //添加监听事件
+        drawerlayout.addDrawerListener(actionBarDrawerToggle);
+
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtils.show(ToolbarDrawerLayoutActivity.this, "btnOne", Toast.LENGTH_SHORT);
+                drawerlayout.closeDrawers();
+            }
+        });
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtils.show(ToolbarDrawerLayoutActivity.this, "btnTwo", Toast.LENGTH_SHORT);
+                drawerlayout.closeDrawers();
+
+            }
+        });
+        btnThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtils.show(ToolbarDrawerLayoutActivity.this, "btnThree", Toast.LENGTH_SHORT);
+                drawerlayout.closeDrawers();
+
+            }
+        });
+        btnFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtils.show(ToolbarDrawerLayoutActivity.this, "btnFour", Toast.LENGTH_SHORT);
+                drawerlayout.closeDrawers();
+
+            }
+        });
+
     }
 
     private void setToolbarBackMethod() {
