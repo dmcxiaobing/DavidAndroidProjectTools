29c545b8217d875232b664d9f625a7c31394870d
qq986945193 <986945193@qq.com>
2017-03-23 15:35:07 +0800

    修改刷新view

--- app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/view/DavidAutoPullListView.java
+++ app/src/main/java/davidandroidprojecttools/qq986945193/com/davidandroidprojecttools/view/DavidAutoPullListView.java
@@ -19,7 +19,13 @@ import android.widget.TextView;
 
 import davidandroidprojecttools.qq986945193.com.davidandroidprojecttools.R;
 
-
+/*
+ * @Author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @OsChina空间: https://my.oschina.net/mcxiaobing
+ */
 public class DavidAutoPullListView extends ListView {
 
     /**
